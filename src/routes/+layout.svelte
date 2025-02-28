b50aaca2bbda45ccc3ff9167bb6dd126dded1911
Nigel <barrowjr0@gmail.com>
2025-02-28 05:27:49 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../app.css';
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div>
+	{@render children()}
+</div>
+
+<style>
+</style>
