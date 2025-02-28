b011984ed78d6190f318184dc1db1d5c4a98daf4
Nigel <barrowjr0@gmail.com>
2025-02-28 05:52:13 -0500

    Add tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	let activeTab = $state(page.url.searchParams.get('tab') || 'generate');
+
+	function onTabChange() {
+		goto(`/?tab=${activeTab}`);
+	}
+</script>
+
+<main class="w-screen">
+	<section class="container pt-4">
+		<Tabs.Root onValueChange={onTabChange} bind:value={activeTab} class="w-full mx-auto">
+			<Tabs.List class="w-full">
+				<Tabs.Trigger class="w-1/2" value="generate">Generate</Tabs.Trigger>
+				<Tabs.Trigger class="w-1/2" value="scan">Scan</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="generate"></Tabs.Content>
+			<Tabs.Content value="scan"></Tabs.Content>
+		</Tabs.Root>
+	</section>
+</main>
