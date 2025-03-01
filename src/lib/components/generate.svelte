3f660242bc73fabdfd6225f63dd0cbf31567ef8d
Nigel Barrow <barrowjr0@gmail.com>
2025-03-01 02:03:55 -0500

    Add size slider

--- src/lib/components/generate.svelte
+++ src/lib/components/generate.svelte
@@ -7,11 +7,14 @@
 	import Download from 'lucide-svelte/icons/download';
 	import * as Select from '$lib/components/ui/select/index.js';
 	import type { QRCodeType } from '@/types';
+	import Slider from './ui/slider/slider.svelte';
 
 	let text = $state('');
 	let ecc: ErrorCorrection = $state('medium');
 	let type: QRCodeType = $state('gif');
-	let scale = $derived((type as QRCodeType) === 'gif' ? 40 : 2);
+	let sliderValue = $state(40);
+	let scale = $derived((type as QRCodeType) === 'gif' ? sliderValue : 2);
+
 	let qrCode = $derived(encodeQR(text, type, { ecc: ecc, border: 2, scale: scale }));
 	let blob = $derived(
 		(type as QRCodeType) === 'gif'
@@ -38,24 +41,30 @@
 			<!-- <Card.Description>Card Description</Card.Description> -->
 		</Card.Header>
 		<Card.Content class="h-full flex flex-col gap-4">
-			<div>
+			<div class="space-y-2">
 				<Label for="text">Text or URL</Label>
-				<Input autofocus bind:value={text} type="text" id="text" maxlength={1000} />
+				<Input class="h-10" autofocus bind:value={text} type="text" id="text" maxlength={1000} />
 			</div>
-			<div>
+			<div class="space-y-2">
 				<Label for="type">Type</Label>
 				<Select.Root bind:value={type} name="type" type="single">
-					<Select.Trigger class="w-full">{typeLabels.get(type)}</Select.Trigger>
+					<Select.Trigger class="w-full h-10">{typeLabels.get(type)}</Select.Trigger>
 					<Select.Content>
 						<Select.Item value="gif">Image</Select.Item>
 						<Select.Item value="svg">SVG</Select.Item>
 					</Select.Content>
 				</Select.Root>
 			</div>
-			<div>
+			{#if type === 'gif'}
+				<div class="space-y-2">
+					<Label for="size">Size: {sliderValue * 25}px</Label>
+					<Slider type="single" bind:value={sliderValue} min={1} max={40} step={1} class="w-full" />
+				</div>
+			{/if}
+			<div class="space-y-2">
 				<Label for="errorCorrection">Error Correction</Label>
 				<Select.Root bind:value={ecc} name="errorCorrection" type="single">
-					<Select.Trigger class="w-full">{ecLabel.get(ecc)}</Select.Trigger>
+					<Select.Trigger class="w-full h-10">{ecLabel.get(ecc)}</Select.Trigger>
 					<Select.Content>
 						<Select.Item value="low">Low (7%)</Select.Item>
 						<Select.Item value="medium">Medium (15%)</Select.Item>
@@ -69,7 +78,7 @@
 			<p>Card Footer</p>
 		</Card.Footer> -->
 	</Card.Root>
-	<Card.Root class="w-1/2">
+	<Card.Root class="w-1/2 flex flex-col justify-between items-center">
 		<Card.Header>
 			<Card.Title>Preview</Card.Title>
 			<!-- <Card.Description>Card Description</Card.Description> -->
@@ -81,7 +90,7 @@
 				<img src={blob} alt={text} />
 			{/if}
 		</Card.Content>
-		<Card.Footer>
+		<Card.Footer class="w-full">
 			<Button download="QR Code" href={blob} variant="outline" class="w-full">
 				<Download class="mr-2 size-4" />
 				Downlaod
